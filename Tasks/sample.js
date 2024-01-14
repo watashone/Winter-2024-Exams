e1137e8ad0a6d88e25aa922cd73709327b909c72
watashone <102485743+watashone@users.noreply.github.com>
2024-01-14 23:08:36 +0200

    Remove unnecessary value assignment

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,13 +1,12 @@
 // Get one random element from an array
 
-// Step 1
-// Add 'use strict';
+// Step 2
+// Remove unnecessary value assignment
 
 'use strict';
 
 const SAMPLE = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr[Math.floor(Math.random() * arr.length)];
+    return arr[Math.floor(Math.random() * arr.length)];
 };
 
 module.exports = SAMPLE;

7c8f7288f40875301a13ed8d085d2fae9a736665
watashone <102485743+watashone@users.noreply.github.com>
2024-01-14 18:13:34 +0200

    Add 'use strict', add const and let, remove empty blocks

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,20 @@
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
-    [];
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
-  ({});
-  return sum;
+// Step 1
+// Add 'use strict';
+// Add const and let
+// Remove empty blocks
+
+'use strict';
+
+const count = (obj) => {
+    let sum = 0;
+    const keys = Object.keys(obj);
+    keys.forEach((key) => {
+        const value = obj[key];
+        if (typeof value === 'number') sum += value;
+    });
+    return sum;
 };
 
 module.exports = count;

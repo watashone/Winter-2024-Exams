b4a801521ffd2384740deb4898aeca3ea2af576a
watashone <102485743+watashone@users.noreply.github.com>
2024-01-14 21:13:49 +0200

    Add 'use strict', add const and let, change identifiers name

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,22 @@
 // Filter array by type name
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
+// Step 1
+// Add 'use strict';
+// Change identifiers name
+// Add const and let
+
+'use strict';
+
+const Filter = (arr, type) => {
+  const remove = [];
+  for (let C of arr) {
+    let x = arr.indexOf(C);
+    if (typeof arr[x] !== type) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
-  return T;
+  for (let x of remove) arr.splice(x, 1);
+  return arr;
 };
 
 module.exports = Filter;

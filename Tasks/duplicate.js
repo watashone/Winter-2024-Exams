655acd1937ec455107c42d29bf4b93462b5b4f04
watashone <102485743+watashone@users.noreply.github.com>
2024-01-14 17:44:20 +0200

    Fix formatting, Add 'use strict', Add const

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,21 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
+// Step 1
+// Fix formatting
+// Add const
+// Add 'use strict';
+
+'use strict';
+
+const duplicate = (value, N) => {
+    if (N <= 0) return [];
+    else {
+        const res = [];
+        for (let i = 0; i < N; i++) {
+            res[i] = value;
+        }
+        return res;
     }
-    return res;
-  }
 };
 
 module.exports = duplicate;

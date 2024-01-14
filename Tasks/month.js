03c7a84908de2df7271b51b8a124dde682f006ab
watashone <102485743+watashone@users.noreply.github.com>
2024-01-14 15:44:00 +0200

    Fix formatting, add 'const' and let, add 'use strict'

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,26 +1,33 @@
 // Get month number
 
-Months = [
-  'jan',
-  'feb',
-  'mar',
-  'apr',
-  'may',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'oct',
-  'nov',
-  'dec',
+// Step 1
+// Add 'use strict';
+// Fix formatting
+// Add 'const' and 'let'
+
+'use strict';
+
+const Months = [
+    'jan',
+    'feb',
+    'mar',
+    'apr',
+    'may',
+    'jun',
+    'jul',
+    'aug',
+    'sep',
+    'oct',
+    'nov',
+    'dec',
 ];
 
-Month = (s) => {
-  l = Months.length;
-  for (let i = 0; i < l; i++) {
-    if (s.toLowerCase().startsWith(Months[i])) return i + 1;
-  }
-  return -1;
+const Month = (s) => {
+    const l = Months.length;
+    for (let i = 0; i < l; i++) {
+        if (s.toLowerCase().startsWith(Months[i])) return i + 1;
+    }
+    return -1;
 };
 
 module.exports = Month;

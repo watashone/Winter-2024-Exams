e1a22c3bce579813b012a56df0901d7050abab2f
watashone <102485743+watashone@users.noreply.github.com>
2024-01-14 21:59:16 +0200

    Add 'use strict', delete empty blocks, add const and let

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,25 +1,23 @@
 // Find an intersection of two dictionaries
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
-  {
-  }
-  for (attribute_name of first_keys) {
-    if (object_1[attribute_name] === object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
-    } else {
-      delete object_1[attribute_name];
-      {
-      }
+// Step 1
+// Add 'use strict';
+// Add const and let
+// Delete empty blocks
+// Fix formatting
+
+'use strict';
+
+const intersection = function intersection(object_1, object_2) {
+    const first_keys = Object.keys(object_1);
+    for (let attribute_name of first_keys) {
+        if (object_1[attribute_name] === object_2[attribute_name]) {
+            object_2[attribute_name] = object_1[attribute_name];
+        } else {
+            delete object_1[attribute_name];
+        }
     }
-    {
-    }
-  }
-  {
-  }
-  return object_1;
+    return object_1;
 };
 
 module.exports = intersection;

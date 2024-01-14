3b5cab8b0fb5d9cfc389146b399deab947f7481a
watashone <102485743+watashone@users.noreply.github.com>
2024-01-14 23:25:45 +0200

    Remove unnecessary return

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,18 +1,15 @@
-// Generate random integer value in given range
+  // Generate random integer value in given range
 
-// Step 1
-// Add 'use strict';
-// Add const
+  // Step 2
+  // Remove unnecessary return
 
-'use strict';
+  'use strict';
 
-const Random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
+  const Random = (min, max) => {
+    if (max === undefined) {
+      max = min;
+    }
     return min + Math.floor(Math.random() * (max - min + 1));
-  }
-};
+  };
 
-module.exports = Random;
+  module.exports = Random;

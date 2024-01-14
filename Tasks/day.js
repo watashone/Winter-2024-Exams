128666ad04171eec03c71a681e8b5a4acfbbb4d1
watashone <102485743+watashone@users.noreply.github.com>
2024-01-14 17:25:38 +0200

    Delete unnecessary for loop, Use method findIndex()
    Use ternary operator, Add const variable

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,22 +1,18 @@
 // Get day number
 
-// Step 1
-// Add 'use strict';
-// Add const and let
-// Delete unnecessary identifiers
-// Change identifiers names
+// Step 2
+// Delete unnecessary for loop
+// Add const variable
+// Use method findIndex()
+// Use ternary operator
 
 'use strict';
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const _parse_day_ = (s) => {
-  for (let i = 0; i < days.length; i++) {
-    if (s.startsWith(days[i].toLowerCase())) {
-      return i + 1;
-    }
-  }
-  return -1;
+    const dayIndex = days.findIndex(day => s.startsWith(day.toLowerCase()));
+    return dayIndex !== -1 ? dayIndex + 1 : -1;
 };
 
 module.exports = _parse_day_;
